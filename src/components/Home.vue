880f118c8c7ec94443b0b8723505f06c671ecd8c
Matheus Matos <matheusdailha@outlook.com>
2020-03-06 09:36:21 -0300

    butao alugar e carrinho

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,14 +20,14 @@
  
    <div class="row">
  
-     <div class="col-md-4"  v-bind:key="filme.id" v-for="filme in filmes">
+     <div class="col-md-3"  v-bind:key="filme.id" v-for="filme in filmes">
        <div class="card">
          <img v-bind:src="filme.imagem" class="card-img-top" alt="imagem do filme">
          <div class="card-body">
            <h5 class="card-title">{{ filme.titulo }}</h5>
            <p class="card-text">{{ filme.descricao }}</p>
            <p class="card-text">{{ filme.valor | formatarPreco("R$") }}</p>
-           <a href="#" class="btn btn-primary">ALUGAR</a>
+           <a href="#" @click="adicionarAoCarrinho(filme)" class="btn btn-primary">Alugar</a>
          </div>
        </div>
      </div>
@@ -46,6 +46,7 @@ export default {
  name: "Home",
  data: function() {
    return {
+    
      title: "Locadora de Filmes",
      horas: new Date().getHours(),
      filmes: [
@@ -54,10 +55,20 @@ export default {
        { id: 3, titulo: "Homem-Formiga", descricao: "Um filme de formigas", valor: 20, imagem: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTMEDMzGG1lkpnENo3hRNA6TSyZqWuGKc2GR0qg6z3j8Ca3AYlx" },
        { id: 4, titulo: "Capitã Marvel", descricao: "Um filme de capitãs", valor: 40, imagem: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTMEDMzGG1lkpnENo3hRNA6TSyZqWuGKc2GR0qg6z3j8Ca3AYlx" },
        { id: 5, titulo: "Hulk", descricao: "Um filme de força", valor: 10, imagem: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTMEDMzGG1lkpnENo3hRNA6TSyZqWuGKc2GR0qg6z3j8Ca3AYlx" }
-     ]
-   };
+     ],
+     carrinho:[],
+     }
+   }, methods: {
+       adicionarAoCarrinho: function(filme){
+         this.carrinho.push(filme.id);
+       },
+       computed:{
+         quantidadeNoCarrinho: function(){
+           return this.carrinho.length;
+         }
+       }
   }
-};
+  }
 
 </script>
 
